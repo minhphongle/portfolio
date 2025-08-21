25038400a7b79e98b988a9a389d4eebc8bd5a340
Le Minh Phong <e0960547@u.nus.edu>
2025-08-22 00:08:54 +0800

    tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,9 +12,19 @@ const config: Config = {
         'indie-flower': ['var(--font-indie-flower)'],
         'custom': ['YourCustomFont', 'sans-serif'], // Add your custom font
       },
+      height: {
+        'mobile-window': 'calc(100vh - 210px)',
+        'mobile-about': 'calc(100vh - 240px)',
+        'mobile-stack': 'calc(100vh - 190px)',
+      },
+      maxHeight: {
+        'mobile-window': 'calc(100vh - 210px)',
+        'mobile-about': 'calc(100vh - 240px)',
+        'mobile-stack': 'calc(100vh - 190px)',
+      },
     },
   },
   plugins: [],
 };
 
-export default config; 
+export default config;
